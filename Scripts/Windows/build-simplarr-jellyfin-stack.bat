b65b0cafdceb8f4d8febaebe8d9bb4e1beabb44f
SuperJohan64 <60057644+SuperJohan64@users.noreply.github.com>
2026-08-29 15:50:30 -0400

    Removed Windows Scripts

--- Scripts/Windows/build-simplarr-jellyfin-stack.bat
+++
@@ -1,2 +0,0 @@
-docker compose -f "%~dp0arr.yml" -f "%~dp0jellyfin.yml" --env-file "%~dp0simplarr-stack.env" up -d
-PAUSE
--- Scripts/Windows/build-simplarr-plex-stack.bat
+++
@@ -1,2 +0,0 @@
-docker compose -f "%~dp0arr.yml" -f "%~dp0plex.yml" --env-file "%~dp0simplarr-stack.env" up -d
-PAUSE
